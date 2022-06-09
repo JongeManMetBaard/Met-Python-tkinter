b9e2f507a6777057ce88e84333f2db1429bb2d0f
ali <ali.gout.12@hotmail.com>
2022-06-09 09:31:05 +0200

    aan/uit

--- lightswitch.py
+++ lightswitch.py
@@ -1,11 +1,24 @@
 import tkinter as tk
 window = tk.Tk()
 
-button = tk.Button(text='...', bg="white", fg="black")
+button = tk.Button(text='', bg="white", fg="black")
 button.pack(pady = 20, padx = 20)
 
 # schijf hier tussen je code
 
+def updateText(event):
+    button.config(text='Switch light off')
+    button.bind("<Button-1>", oldText)
+    window.configure(bg='yellow')
+    print("light is on")
+
+def oldText(event):
+    button.config(text='Switch light on')
+    button.bind("<Button-1>", updateText)
+    window.configure(bg='black')
+    print("light is off") 
+
+oldText("")
 
 
 # schijf hier tussen je code
